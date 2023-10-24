461751bd6b8fc85c7d4f4e33bbfd64c5c081a50b
endijulian <endijulian080798@gmail.com>
2023-10-24 17:40:16 +0700

    integration dashboard

---
+++ app/Http/Controllers/User/DashboardController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\Movie;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $featureMovies = Movie::whereIsFeatured(true)->get();
+        $movies = Movie::all();
+
+        return Inertia::render('User/Dashboard/Index', [
+            'featureMovies' => $featureMovies,
+            'movies' => $movies,
+        ]);
+        // return Inertia::render('User/Dashboard/Index');
+    }
+}
--- resources/js/Layouts/Authenticated/Index.jsx
+++ resources/js/Layouts/Authenticated/Index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Sidebar from './Sidebar';
 import Topbar from './Topbar';
 
-export default function Authenticated({children}) {
+export default function Authenticated({user,children}) {
   return (
     <>
         <div className="mx-auto max-w-screen hidden lg:block">
@@ -14,7 +14,7 @@ export default function Authenticated({children}) {
             <div className="ml-[300px] px-[50px]">
                 <div className="py-10 flex flex-col gap-[50px]">
                     {/* STAR: Tovbar */}
-                        <Topbar></Topbar>
+                        <Topbar name={user.name}></Topbar>
                     {/* END: Tovbar */}
 
                     <main>{children}</main>
